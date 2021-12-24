a722b563fe907d09d0fdb5b6b84290660df5f72f
Your NameAlex <alehr45@gmail.com>
2021-12-24 13:34:40 -0600

    added create post page and functionality to save posts in database

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+Axios.defaults.baseURL = "http://localhost:8080"
 //Components
 import Home from "./components/Home"
 import Header from "./components/Header"
@@ -8,6 +9,9 @@ import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import Axios from "axios"
+import ViewSinglePost from "./components/ViewSinglePost"
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
@@ -19,6 +23,8 @@ function Main() {
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/post/:id" element={<ViewSinglePost />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
       <Footer />
     </BrowserRouter>
