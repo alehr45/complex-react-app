39f379a94d591c834d9650e193bf6973e260abdd
Your NameAlex <alehr45@gmail.com>
2022-01-18 08:16:17 -0600

    merge to deploy

--- app/Main.js
+++ app/Main.js
@@ -116,13 +116,13 @@ function Main() {
           <Suspense fallback={<LoadingDotsIcon />}>
             <Routes>
               <Route path="/profile/:username/*" element={<Profile />} />
-              <Route path="/" element={<>{state.loggedIn ? <Home /> : <HomeGuest />}</>} />
+              <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
               <Route path="/post/:id" element={<ViewSinglePost />} />
               <Route path="/post/:id/edit" element={<EditPost />} />
               <Route path="/create-post" element={<CreatePost />} />
               <Route path="/about-us" element={<About />} />
               <Route path="/terms" element={<Terms />} />
-              <Route element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </Suspense>
           <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
